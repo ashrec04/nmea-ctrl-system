48c60b16f5fd550846c7578c611fd9496226a438
Ash <ashrec23@gmail.com>
2026-02-03 16:05:31 +0000

    mesages send from windows recieved by script on pi

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-from listener import NMEAListener
+import subprocess
+import os
 
 def main():
-    NMEAListener().start()
-    
+    #===================================#
+    # Read data sent from CAN USB Port  #
+    #===================================#
+
+    project_dir = "USB-CAN-A"  # Path to waveshare USB-CAN code
+    os.chdir(project_dir)   # change directory to folder
+    subprocess.run(["sudo", "make", "clean"], check=True)
+    subprocess.run(["sudo", "make"], check=True)
+
+    #run the canusb program command
+    # sudo ./canusb -t -d /dev/ttyUSB0 -s 125000 -t
+    subprocess.run([
+        "sudo", "./canusb",
+        "-t",
+        "-d", "/dev/ttyUSB0",   # TTY device used
+        "-s", "125000",         # CAN Baud rate 125K for NMEA2000
+        "-t"
+    ], check=True)
+
 
 if __name__ == "__main__":
     main()
