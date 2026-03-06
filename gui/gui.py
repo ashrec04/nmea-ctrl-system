c15af46974701866b574d710968844461b64166e
ashrec04 <ashrec04@github.com>
2026-03-06 16:14:31 +0000

    Global dictionary added to prevent hard coding

--- gui/gui.py
+++ gui/gui.py
@@ -11,6 +11,13 @@ WINDOW_PATH = 'gui/resources/mainwindow.ui'
 LIGHT_BLUE = "#E3F6FD"
 DARK_BLUE = "#0B76A0"
 TEAL_GREEN = "#1AA5A2"
+
+GRAPH_META = {
+  "128267": {"title": "Water Depth", "axis": "Depth (m)"},
+  "129026": {"title": "Speed Over Ground", "axis": "Speed (Knots)"},
+  "130306": {"title": "Wind Data", "axis": "Speed (Knots)"}
+}
+
 #~~
 
 # Subclass QMainWindow to customise the window
@@ -35,7 +42,6 @@ class MainWindow(QMainWindow):
         self.graph_widgets = {}
         self.graph_columns = 1
 
-        self.graph_time = 0
         self.graphGridLayout.setContentsMargins(12, 12, 12, 12)
         self.graphGridLayout.setHorizontalSpacing(12)
         self.graphGridLayout.setVerticalSpacing(12)
@@ -50,62 +56,58 @@ class MainWindow(QMainWindow):
 
 
     def AddGraph(self, pgn):
-        graph_name = str(pgn)
-
         plot_graph = pg.PlotWidget()
-        plot_graph.setObjectName(graph_name)
+        plot_graph.setObjectName(pgn)
         plot_graph.setMinimumHeight(260)
         plot_graph.setSizePolicy(
             QtWidgets.QSizePolicy.Policy.Expanding,
             QtWidgets.QSizePolicy.Policy.Expanding,
         )
-        # plot_graph.setStyleSheet(f"border: 2px solid {DARK_BLUE};")
 
         graph_index = len(self.graph_widgets)
         row = graph_index // self.graph_columns
         column = graph_index % self.graph_columns
-        self.graphGridLayout.addWidget(plot_graph, row, column)  # put in grid
 
+        self.graphGridLayout.addWidget(plot_graph, row, column)  # put in grid
         self.graphGridLayout.setRowStretch(row, 1)
         self.graphGridLayout.setColumnStretch(column, 1)
 
         plot_graph.show()
 
+        graph_meta = GRAPH_META.get(pgn, {"title": f"PGN {pgn}", "axis": "Value"})
+
         plot_graph.setBackground("w")
-        plot_graph.setTitle(graph_name, **self.title_style)
-        plot_graph.setLabel("left", "Depth (m)", **self.axis_style) # y axis
+        plot_graph.setTitle(graph_meta["title"], **self.title_style)
+        plot_graph.setLabel("left", graph_meta["axis"], **self.axis_style) # y axis
         plot_graph.setLabel("bottom", "Time", **self.axis_style) # x axis
+        plot_graph.getAxis("bottom").enableAutoSIPrefix(False)
 
         plot_graph.showGrid(x=True, y=True)
-        plot_graph.setYRange(0, 40)
 
         graph_line = plot_graph.plot(
             [], # x plot
             [], # y plot
             pen=self.pen # line style
         )
-        
-        self.graph_data[graph_name] = {"x": [], "y": [], "line": graph_line}
-        self.graph_widgets[graph_name] = plot_graph
+
+        self.graph_data[pgn] = {"x": [], "y": [], "line": graph_line}
+        self.graph_widgets[pgn] = plot_graph
 
 
-    def AddGraphPlot(self, graph_name, value):
-
-        graph_name = str(graph_name)
-        g = self.graph_data.get(graph_name)
+    def AddGraphPlot(self, pgn, value):
+        g = self.graph_data.get(pgn)
 
         try:
             y_value = float(value)
-        except (TypeError, ValueError):
+        except (TypeError, ValueError) as e:
+            print("ADDING PLOT ERROR: ", e)
             return
 
-        g["x"].append(self.graph_time)
+        g["x"].append(len(g["x"]))
         g["y"].append(y_value)
 
-        self.graph_time += 1
-
         g["line"].setData(g["x"], g["y"])
-        plot_graph = self.graph_widgets.get(graph_name)
+        plot_graph = self.graph_widgets.get(pgn)
 
         if plot_graph is not None:
             plot_graph.enableAutoRange(axis="x", enable=True)
@@ -113,6 +115,4 @@ class MainWindow(QMainWindow):
             plot_graph.update()
             plot_graph.repaint()
 
-        self.graph_data[graph_name] = g        
-    
-    
+        self.graph_data[pgn] = g
